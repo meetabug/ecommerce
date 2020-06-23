e430eed94441d8d9e7b4758a9e4f5eab7ffe32e5
meetabug <1272526724@qq.com>
2020-06-23 22:35:54 +0800

    Products Section Part 1

--- app/Models/AttributeValue.php
+++ app/Models/AttributeValue.php
@@ -20,4 +20,9 @@ class AttributeValue extends Model
     {
         return $this->belongsTo(Attribute::class);
     }
+
+    public function productAttributes()
+    {
+        return $this->belongsToMany(ProductAttribute::class);
+    }
 }
