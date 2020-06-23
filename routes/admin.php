8d276d6ecd476d135d6ea4299105d4a814d3755f
meetabug <1272526724@qq.com>
2020-06-23 14:21:46 +0800

    Settings Section Part 2

--- routes/admin.php
+++ routes/admin.php
@@ -10,4 +10,7 @@ Route::group(['prefix' => 'admin'], function () {
             return view('admin.dashboard.index');
         })->name('admin.dashboard');
     });
+
+    Route::get('/settings', 'Admin\SettingController@index')->name('admin.settings');
+    Route::post('/settings', 'Admin\SettingController@update')->name('admin.settings.update');
 });
