1b3fa0e2216e8585a5c3e813ab3541bcdba5d5b6
meetabug <1272526724@qq.com>
2020-06-23 21:52:28 +0800

    Attributes Section Part 3

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,5 +15,5 @@ mix.copyDirectory('resources/backend', 'public/backend');
 mix.copyDirectory('resources/frontend', 'public/frontend');
 
 
-// mix.js('resources/js/app.js', 'public/js')
-//     .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/backend/js');
+
