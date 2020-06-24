9a26e58635e910cb7d9c5d57915fe9d836885f3c
meetabug <1272526724@qq.com>
2020-06-24 22:42:37 +0800

    Product Details Page

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'site.pages.homepage');
 Route::get('/category/{slug}', 'Site\CategoryController@show')->name('category.show');
 Route::get('/product/{slug}', 'Site\ProductController@show')->name('product.show');
+Route::post('/product/add/cart', 'Site\ProductController@addToCart')->name('product.add.cart');
 
 Auth::routes();
 require  'admin.php';
